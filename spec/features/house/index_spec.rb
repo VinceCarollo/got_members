f5831742b9d8ecc7ca48563dccf8fb20f931e218
Vince Carollo <vincecarollo@gmail.com>
2019-07-19 11:15:34 -0600

    Add more house options to drop down and capitilize house names to choose from

--- spec/features/house/index_spec.rb
+++ spec/features/house/index_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe "As a visitor" do
     it "can search for houses" do
       visit root_path
 
-      select "greyjoy", :from => "house"
+      select "Greyjoy", :from => "house"
       click_button "Get Members"
 
       expect(current_path).to eq(search_path)
-      expect(current_url).to include('house=greyjoy')
+      expect(current_url).to include('house=Greyjoy')
 
       expect(page).to have_content('7 Members')
       expect(page).to have_css('.member', count: 7)
