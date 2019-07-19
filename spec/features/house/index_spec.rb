b57aac0a0160b6094a1281a5c3b910d3e25250a0
Vince Carollo <vincecarollo@gmail.com>
2019-07-19 10:42:37 -0600

    Start to change apis

--- spec/features/house/index_spec.rb
+++ spec/features/house/index_spec.rb
@@ -3,15 +3,6 @@ require 'rails_helper'
 RSpec.describe "As a visitor" do
   context 'on the home page' do
 
-    # As a user
-    # When I visit "/"
-    # And I select "Greyjoy" from the dropdown
-    # And I click on "Get Members"
-    # Then my path should be "/search" with "house=greyjoy" in the parameters
-    # And I should see a message "7 Members"
-    # And I should see a list of the 7 members of House Greyjoy
-    # And I should see a name and id for each member.
-
     it "can search for houses" do
       visit root_path
 
